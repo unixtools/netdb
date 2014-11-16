0490fa1f00735527e684142c1e0c9ef3af194d4b
root <root@itapps-d1.srv.mst.edu>
2014-11-16 10:07:21 -0600

    add bootstrap and json editor

---
+++ html/js/json-editor-0.7.13/src/intro.js
@@ -0,0 +1,12 @@
+/*! JSON Editor v0.7.13 - JSON Schema -> HTML Editor
+ * By Jeremy Dorn - https://github.com/jdorn/json-editor/
+ * Released under the MIT license
+ *
+ * Date: 2014-11-11
+ */
+
+/**
+ * See README.md for requirements and usage info
+ */
+
+(function() {
