88f28f7c8ef9ea8c2a451983b095dd54e7a91baf
nneul on skyhawk.home.neulinger.org <nneul@skyhawk.home.neulinger.org>
2015-10-07 18:28:36 +0000

    update dhcp server list

--- libs/NetMaint/Config.pm
+++ libs/NetMaint/Config.pm
@@ -40,7 +40,8 @@ our $NETDB_DEFAULT_NOTIFY = "nneul\@neulinger.org";
 
 our $NETDB_DB_HOST      = "netmgr.spirenteng.com";
 our $NETDB_DNS_SERVERS  = ["netmgr.spirenteng.com"];
-our $NETDB_DHCP_SERVERS = [ "fc-dhcp-ito.spirenteng.com" ];
+# Why is this not pulling from database?!
+our $NETDB_DHCP_SERVERS = [ "ub-dhcp1-ito.spirenteng.com", "ub-dhcp2-ito.spirenteng.com" ];
 
 our $NETDB_DISABLE_BACKLOG_REPORT = 1;
 
