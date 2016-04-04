0778b0a4cefd18a57c8a695bbb67f0f941fe5ea4
nneul on mst.edu <nneul@mst.edu>
2016-04-04 11:36:52 -0500

    add field to track last size/lines

--- schema/mysql-netdb-updates.sql
+++ schema/mysql-netdb-updates.sql
@@ -7,3 +7,11 @@ alter table dns_ptr add column etime datetime;
 alter table dns_srv add column etime datetime;
 alter table dns_txt add column etime datetime;
 alter table dns_spf add column etime datetime;
+
+alter table dns_soa add column last_size integer default 0;
+alter table dns_soa add column last_lines integer default 0;
+update dns_soa set last_size=0;
+update dns_soa set last_lines=0;
+alter table dns_soa modify column last_size integer default 0 not null;
+alter table dns_soa modify column last_lines integer default 0 not null;
+
